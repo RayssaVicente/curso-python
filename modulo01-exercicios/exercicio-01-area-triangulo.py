c387e0cd537058c44732a91b95c71f2ce6bb5780
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2024-10-25 12:14:41 -0300

    exercio-01 Calculando a área de um triangulo
    Nesse exercício está basicamente a pratica do print e input

---
+++ modulo01-exercicios/exercicio-01-area-triangulo.py
@@ -0,0 +1,9 @@
+#Implemente em Python, um
+#algoritmo capaz de calcular
+#a área de um triângulo.
+
+print("Para calcular a área do traingulo:")
+base = int(input("Digite a base do triangulo: "))
+altura = int(input("Digite a altura do triangulo: "))
+area_triangulo = (base*altura)/2
+print(f"A área do triangulo é {area_triangulo}")
