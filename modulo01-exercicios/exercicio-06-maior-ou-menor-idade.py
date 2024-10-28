f60707a0a1df55592ec8df1981dbffd02a022dcf
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2024-10-28 16:28:21 -0300

    exercício de verificação de idade
    Nesse exercício foi usado as estruturas condicionais para ver se uma pessoa e ou não menor de idade.

---
+++ modulo01-exercicios/exercicio-06-maior-ou-menor-idade.py
@@ -0,0 +1,9 @@
+#Escreva um programa que recebe a
+#idade de um usuário e verifica se ele é
+#maior de idade.
+
+idade = int(input("Digite sua idade: "))
+if idade < 18:
+    print("Você é menor de idade")
+else:
+    print("Você é maior de idade")
